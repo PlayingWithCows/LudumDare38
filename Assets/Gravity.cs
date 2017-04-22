bb54ee26605d4e25800949538cfcde21300d6cf7
PlayingWithCows <wiealbern@gmail.com>
2017-04-22 15:27:10 +0200

    Created Sun and first planet as well as player and a few basic scripts for player gravity and making player stand upright on the planet

---
+++ Assets/Gravity.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+    public float gravity;
+    public Rigidbody rigidbodyComponent;
+
+	void FixedUpdate () {
+        rigidbodyComponent.AddRelativeForce(0, -gravity, 0);
+	}
+}
---
+++ Assets/StandUpright.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandUpright : MonoBehaviour {
+    public GameObject planetYouStandOn;
+	
+	void FixedUpdate () {
+        StandUp();
+	}
+
+    private void StandUp()
+    {
+        //Vector3 direction = planetYouStandOn.transform.position - transform.position;
+        //Quaternion toRotation = Quaternion.FromToRotation(-transform.up, direction);
+        //transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, 1 * Time.time);
+
+        transform.LookAt(planetYouStandOn.transform.position);
+        transform.Rotate(new Vector3(-1.0f, 0, 0), 90);
+    }
+}
