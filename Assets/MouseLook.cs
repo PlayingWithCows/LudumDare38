2523852a666254f42b1597cea673024780e68a6f
PlayingWithCows <wiealbern@gmail.com>
2017-04-22 17:55:06 +0200

    cobbled together some stuff, doesn't work right

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MouseLook : MonoBehaviour
+{
+    	public float mouseSensitivity;
+	public float upDownRange = 60f;
+
+	private float vRot = 0;
+
+	void Update () {
+
+		
+			float rotLeftRight = Input.GetAxis ("Mouse X") * mouseSensitivity;
+			transform.Rotate (0, rotLeftRight, 0,Space.Self);
+	
+			vRot -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+			vRot = Mathf.Clamp (vRot, -upDownRange, upDownRange);
+			Camera.main.transform.localRotation = Quaternion.Euler (vRot, 0, 0);
+		
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    private CharacterController myController;
+
+
+    public float moveSpeed;
+    float verticalVelocity = 0;
+
+    public Transform headCamera;
+    public float jumpSpeed;
+    public float gravity;
+
+    private void Start()
+    {
+        myController = gameObject.GetComponent<CharacterController>();
+    }
+    void FixedUpdate () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    
+        float forwardSpeed = Input.GetAxis("Horizontal") * moveSpeed;
+        float sideSpeed = Input.GetAxis("Vertical") * moveSpeed;
+        if (!myController.isGrounded)
+        {
+            verticalVelocity += -gravity * Time.deltaTime;
+        }
+        Vector3 speed = new Vector3(forwardSpeed, verticalVelocity, sideSpeed);
+
+        speed = transform.rotation * speed;
+
+        myController.Move(speed * Time.deltaTime);
+
+    }
+
+    public void Jump()
+    {
+
+        if (myController.isGrounded)
+            verticalVelocity = jumpSpeed;
+
+    }
+}
